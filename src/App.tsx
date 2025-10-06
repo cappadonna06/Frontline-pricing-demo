0468630805b2d783ab3a9dc2da8680a99a0f7749
cappadonna06 <msells.caltech@gmail.com>
2025-10-06 10:04:43 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FrontlinePricingCalculator from "./FrontlinePricingCalculator";
+
+export default function App() {
+  return (
+    <div className="p-6 max-w-7xl mx-auto space-y-6">
+      <FrontlinePricingCalculator />
+    </div>
+  );
+}
